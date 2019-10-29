d7d3e26f7471e48ead080cb126d8b87e505a39a9
fkdaa <56007469+fkdaa@users.noreply.github.com>
2019-10-29 09:46:41 +0900

    Update and rename ledon.py to ledblink.py

---
+++ ledblink.py
@@ -0,0 +1,11 @@
+﻿import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14, GPIO.OUT)
+
+while 1
+  GPIO.output(14, GPIO.HIGH)
+  time.sleep(0.5)
+  GPIO.cleanup()
+  time.sleep(0.5)
